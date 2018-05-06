a1e1a2413a98ee2077e456e9f3adfe90db8d2a93
Vladislav Lipatov <lipatov.xex@gmail.com>
2018-05-06 14:56:15 +0300

    requests for albums

--- src/components/ArtistPage/index.js
+++ src/components/ArtistPage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 
 const Album = ({ img, name, id }) => {
-    return <div className='album'>
+    return <div className='album' style={{ backgroundImage: `url(${img})` }}>
         <p>{name}</p>
     </div>
 }
@@ -11,6 +11,28 @@ export default class ArtistPage extends React.Component {
         albums: [],
         videos: []
     }
+    async fetchAlbums() {
+        let response = await fetch(`https://itunes.apple.com/lookup?id=${this.props.artistId}&entity=album`)
+        if (response.status === 200) {
+            let data = await response.json()
+            console.log(data)
+            let albums = []
+            data.results.map(item => {
+                if (item.wrapperType !== 'artist') {
+                    albums.push({
+                        id: item.collectionId,
+                        name: item.collectionName,
+                        img: item.artworkUrl100
+                    })
+                }
+            })
+            this.setState({ albums: albums })
+
+        }
+    }
+    componentDidMount() {
+        this.fetchAlbums()
+    }
     render() {
         let { name, genre, id } = this.props
         return < div className='artist-page' >
@@ -18,7 +40,7 @@ export default class ArtistPage extends React.Component {
             <p>{genre}</p>
             <div className='artist-albums'>
                 {this.state.albums.map(album => {
-                    return <Album name={album.name} img={album.img} />
+                    return <Album key={album.id} name={album.name} img={album.img} />
                 })}
             </div>
             <div className='artist-videos'>
